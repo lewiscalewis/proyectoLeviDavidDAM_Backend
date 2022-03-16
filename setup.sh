861851411b5f7fad08b0a800fd7a16d95d193301
DavidPerezContreras <91068383+DavidPerezContreras@users.noreply.github.com>
2022-03-16 12:40:06 -0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,16 +1,54 @@
+#Join hamachi network
+sudo apt install logmein-hamachi
+sudo hamachi login
+sudo hamachi join 465-709-324
+#Te pedirá la contraseña de la red: estareddasida
 
 
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Solo para el desarrollo.
+################################################
+
+
+
+
+#Instalar y configurar mysql
 sudo apt install mysql-server
-#Configure mysql-sqver. to do that login as root user
+
 sudo mysql
 
 
-#Once logged in the mysql shell.Change root user's password.
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+#Cambiamos la contraseña del usuario root. pasará a ser "root".
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'proyectolevidaviddam';
 
 #Aplicamos las modificaciones en los permisos.
 FLUSH PRIVILEGES;
 
 
+
+
+
+#para el control de versiones
+sudo apt install git
+
+
+#install nodejs
+sudo apt install nodejs
+
+
+#install npm 
+sudo apt install npm
+
+
 #para conectarnos a la base de datos en main.js necesitamos la dependencia.
 npm install mysql
+
+#mas dependencias del npm
+npm install cors
+npm install express
+npm install http
+
+
+
+
+
+
