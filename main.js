bd23d760477bcb2b21950cc5ad6e19472c617182
lewiscalewis <lewisvicna@gmail.com>
2022-03-23 22:15:59 +0100

    probando con body parser

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const express = require('express');
+var body_parser = require('body-parser');
 const cors = require('cors');
 
 const path = require('path');
 const app = express();
 var http = require('http').createServer(express);
+app.use(body_parser.urlencoded({extended:true}));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
