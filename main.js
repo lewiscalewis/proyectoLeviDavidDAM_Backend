7d79c97c49fef3159cfa812ef354c2fd57409426
lewiscalewis <lewisvicna@gmail.com>
2022-03-23 23:28:56 +0100

    probando cambios

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const express = require('express');
-var body_parser = require('body-parser');
 const cors = require('cors');
-
-const path = require('path');
+const mysql = require('mysql');
 const app = express();
 var http = require('http').createServer(express);
-app.use(body_parser.urlencoded({extended:false}));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-app.use(body_parser.json());
+app.use(express.json());
 
 //#############################################################################################
 
