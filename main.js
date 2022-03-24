6ca97514698ce568032c25fa03368775b67d0094
lewiscalewis <lewisvicna@gmail.com>
2022-03-24 01:24:40 +0100

    ya vaaaa

--- main.js
+++ main.js
@@ -41,9 +41,8 @@ app.post('/signup/', (req, res)=>{
             console.error(error);
             res.status(500).end();
         } else {
-            console.log(req.body)
             console.log(req.body.email+" y "+result)
-            if(result == req.body.email) {
+            if(result[0] == req.body.email) {
                 console.error("El usuario con el correo "+req.body.email+" ya está registrado")
                 res.send("mail_error");
             } else {
