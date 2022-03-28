df889a7a66bfd519e7a8c821883c6730e599ff4a
lewiscalewis <lewisvicna@gmail.com>
2022-03-28 21:00:23 +0200

    cambio menor para seleccionar bbdd jardineria (antes "proyecto")

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "proyectolevidaviddam",
-  database: "proyecto"
+  database: "jardineria"
 });
 
 /*Sample query*/
