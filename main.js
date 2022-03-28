2a6a81dd57b5b21e1621a37c997b39bc25765c75
lewiscalewis <lewisvicna@gmail.com>
2022-03-28 21:18:14 +0200

    cambiado para usar bbdd proyecto

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "proyectolevidaviddam",
-  database: "jardineria"
+  database: "proyecto"
 });
 
 /*Sample query*/
