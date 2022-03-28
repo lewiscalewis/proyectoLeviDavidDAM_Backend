15192a9daa58b90d6dfd3e774d04e534beadcafb
lewiscalewis <lewisvicna@gmail.com>
2022-03-28 23:26:40 +0200

    YA SI DEBERIA DE FUNCIONAR EL REGISTRAR USAURIO

--- main.js
+++ main.js
@@ -35,7 +35,8 @@ app.post('/users/', (req, res)=>{
 });
 
 app.post('/signup/', (req, res)=>{
-    connection.query('INSERT INTO Users (name, surname, email, password, username) VALUES (?, ?, ?, ?, ?)'[req.body.nombre, req.body.apellido, req.body.correo, req.body.contraseña], (error, result)=>{
+    console.log(req.body.name, req.body.surname, req.body.email, req.body.password)
+    connection.query('INSERT INTO Users (name, surname, email, password, username) VALUES (?, ?, ?, ?, ?)'[req.body.name, req.body.surname, req.body.email, req.body.password], (error, result)=>{
         if(error){
             console.error(error)
             res.status(500).end()
