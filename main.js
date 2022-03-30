4d61a00726551a033382d47b7e71659e74cf3e52
lewiscalewis <lewisvicna@gmail.com>
2022-03-30 23:03:10 +0200

    probando jwt

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
 const app = express();
+var jwt = require('jsonwebtoken');
 var http = require('http').createServer(express);
 app.use(cors());
 var bodyParser = require('body-parser')
@@ -11,6 +12,36 @@ app.use(bodyParser.urlencoded({ extended: true }))
 //app.use(express.json());
 
 //#############################################################################################
+
+// Clave para cifrado de JWT
+const config = {
+	llave : "proyectolevidaviddam"
+};
+
+app.set('llave', config.llave);
+
+//MiddleWare seguridad token
+const rutasProtegidas = express.Router(); 
+rutasProtegidas.use((req, res, next) => {
+    const token = req.headers['access-token'];
+	
+    if (token) {
+      jwt.verify(token, app.get('llave'), (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Token inválido' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token no proveído.' 
+      });
+    }
+ });
+
+//------------------------------------
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -42,10 +73,53 @@ app.post('/signup/', (req, res)=>{
             res.status(500).end()
         }else{
             console.log(`Usuario ${req.body.username} creado correctamente`)
-            res.send("success")
+            res.status(200).end()
         }
     })
 });
+
+app.post('/login', (req, res) => {
+    connection.query('SELECT password, username FROM Users  WHERE username = ? AND password = ?',[req.body.username, req.body.password], (error, result)=>{
+        if(error){
+            console.error(error)
+            res.status(500).end()
+        }else{
+            if(result[0] > 1) {
+                
+                const payload = {
+                    check:  true
+                };
+
+                var currentdate = new Date(); 
+                var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+                const token = jwt.sign(payload, app.get('llave'), {
+                    username: req.body.username,
+                    password: req.body.password,
+                    datetime: datetime,
+                    expiresIn: 1440
+                });
+
+                connection.query('INSERT INTO Users token VALES ?',[token], (error1)=>{
+                    if(error1){
+                        console.error(error1)
+                        res.status(500).end()
+                    }else{
+                        res.status(200).end()
+                    }
+                })
+                res.send(token)
+            } else {
+                res.send("Usuario o contraseña incorrectos")
+            }
+        }
+    })
+})
 
 app.post('/check-email', (req, res)=>{
     connection.query('SELECT email FROM Users WHERE email = ?',[req.body.email], (error, result)=>{
