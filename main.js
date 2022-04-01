432f7055fc1e28e933da587951817213cae2a26a
Leví Vicente <lewisvicna@gmail.com>
2022-04-01 13:22:20 +0200

    puerto cambiado -> 80

--- main.js
+++ main.js
@@ -179,7 +179,7 @@ app.post('/', (req, res)=>{
 });
 
 
-app.listen(11230, () => {
-    console.log('Levi,s app listen on port 11230');
+app.listen(80, () => {
+    console.log('Levi,s app listen on port 80');
 })
 
