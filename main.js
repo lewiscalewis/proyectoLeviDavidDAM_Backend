37d1dc5e0615df1917259ef7c5f2c17e7d5716cb
leví <lewisvicna@gmail.com>
2022-04-21 22:38:04 +0200

    find-users added

--- main.js
+++ main.js
@@ -283,6 +283,23 @@ app.post('/items', rutasProtegidas, (req, res)=>{
     });
 });
 
+app.post('/find-users', rutasProtegidas, (req, res)=>{
+    let query = `SELECT *
+    FROM Users U
+    WHERE
+        U.username LIKE '%?%'`
+    connection.query(query, [req.body.username], (err, resp)=>
+    {
+        if(err){
+            console.log(err)
+            console.log(query)
+            res.status(500).end()
+        }else{
+            res.status(200).send(resp)
+        }
+    });
+});
+
 
 //############################################################################################
 
