aff06c41378554b0ac7e007b2d7e3b12b962ec22
leví <lewisvicna@gmail.com>
2022-04-25 23:05:34 +0200

    sockets en marchaaaa

--- main.js
+++ main.js
@@ -51,8 +51,6 @@ io.on('connection', (socket) => {
         socket.emit("start-room", chat);
         console.log(msg+" en sala "+chat); // world
         io.to(chat).emit("message", msg)
-        socket.to(chat).emit("message", msg)
-        socket.emit("message", msg)
     });
 
 });
