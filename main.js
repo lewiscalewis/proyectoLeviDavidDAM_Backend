6ad32d046711fffc0fa0b7f83a9efdb4bb4b0f5a
DavidPerezContreras <91068383+DavidPerezContreras@users.noreply.github.com>
2022-05-18 20:15:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -819,7 +819,7 @@ app.post('/upload-item', uploadFile.single('file'), rutasProtegidas,  (req, res)
 	console.log("uploadDate= "+uploadDate);
 	console.log("item= "+req.file.filename);
 	
-    connection.query('INSERT INTO Items(name, username, genere, description, copyright, uploadDate, item) VALUES (?, ?, ?, ?, ?, ?, ?)', [req.body.name, req.body.author, req.body.genere, req.body.description, 1, req.body.uploadDate, req.file.filename], (err, response)=>{
+    connection.query('INSERT INTO Items(name, username, genere, description, copyright, uploadDate, item) VALUES (?, ?, ?, ?, ?, ?, ?)', [req.body.name, req.body.author, req.body.genere, req.body.description, "1", req.body.uploadDate, req.file.filename], (err, response)=>{
         if(err){
 		console.log(err)
             res.status(500).end();
