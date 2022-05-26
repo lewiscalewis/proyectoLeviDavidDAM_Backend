4d49b54f7341c6cb66affe666c718515f4f64162
leví <lewisvicna@gmail.com>
2022-05-26 21:16:57 +0200

    experimento

--- main.js
+++ main.js
@@ -420,12 +420,13 @@ app.post('/check-username', (req, res)=>{
 
 //petición para obtener items por medio de la barra de búsqueda
 app.post('/items-search', rutasProtegidas, (req, res)=>{
+    var filtro = req.body.genre == "all" ? "" : "AND I.genre = ?"
     connection.query(
         `SELECT *
         FROM Items I
         WHERE
-            I.name LIKE ? AND
-            I.genre = ?`, ['%'+req.body.item+'%', req.body.genre], (err, resp)=>
+            I.name LIKE ?
+            ${filtro}`, ['%'+req.body.item+'%', req.body.genre], (err, resp)=>
     {
         if(err){
             console.log(err)
